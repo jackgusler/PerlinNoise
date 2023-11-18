7bd1ea6cc3f42b17646b0a9aa84210718457c098
Niklas <niklashallstein1798@gmail.com>
2023-11-18 13:48:02 -0500

    camera controller

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void moveCamera(int pos)
+    {
+        if(pos == 1){
+            transform.position = new Vector3(0, 0, -10);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if(pos == 2){
+            transform.position = new Vector3(0, 10, 0);
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+        else if(pos == 3){
+            transform.position = new Vector3(10, 10, 10);
+            transform.rotation = Quaternion.Euler(30, 225, 0);
+        }
+    }
+}
